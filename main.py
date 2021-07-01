c3a12c72c77254245cc1e66eba45cecc185c03f9
NastyaFilippova <nsfl96@icloud.com>
2021-07-02 00:30:05 +0300

    Исправила ошибку во 2-ом задании

--- main.py
+++ main.py
@@ -7,6 +7,15 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_for_lecturer(self, lecturer, course, lecturer_grade):
+        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
+            if course in lecturer.teach_grades:
+                lecturer.teach_grades[course] += [lecturer_grade]
+            else:
+                lecturer.teach_grades[course] = [lecturer_grade]
+        else:
+            return 'Ошибка'
+
 
 class Mentor:
     def __init__(self, name, surname):
@@ -19,15 +28,6 @@ class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.teach_grades = {}
-
-    def rate_teaching(self, student, course, teach_grade):
-        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
-            if course in self.teach_grades:
-                self.teach_grades[course] += [teach_grade]
-            else:
-                self.teach_grades[course] = [teach_grade]
-        else:
-            return 'Ошибка'
 
 
 class Reviewer(Mentor):
@@ -46,18 +46,16 @@ cool_reviewer.courses_attached += ['Python']
 
 best_student = Student('Nastya', 'Fil', 'female')
 best_student.courses_in_progress += ['Python']
+best_student.courses_in_progress += ['GIT']
 
 cool_lecturer = Lecturer('Mike', 'Mikelson')
 cool_lecturer.courses_attached += ['GIT']
 
-new_student = Student('Nelly', 'Sidorova', 'female')
-new_student.courses_in_progress += ['GIT']
-
 cool_reviewer.rate_hw(best_student, 'Python', '10')
 print(f'Оценка студенту {best_student.name} {best_student.surname}: {best_student.grades}')
 
-cool_lecturer.rate_teaching(new_student, 'GIT', '10')
-print(f'Оценка преподавателю {cool_lecturer.name} {cool_lecturer.surname} от студента {new_student.name} {new_student.surname}: {cool_lecturer.teach_grades}'
+best_student.rate_for_lecturer(cool_lecturer, 'GIT', '10')
+print(f'Оценка лектору {cool_lecturer.name} {cool_lecturer.surname} : {cool_lecturer.teach_grades}'
       )
 
 
