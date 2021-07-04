d6201bd54fdfb682d62e018ce89fb3d89c4d84bc
NastyaFilippova <nsfl96@icloud.com>
2021-07-04 20:58:29 +0300

    Исправила функцию сравнения: Задание 3,часть2

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 class Student:
+    average_rating = 0
+
     def __init__(self, name, surname, gender):
         self.name = name
         self.surname = surname
@@ -25,6 +27,12 @@ class Student:
         for_student = f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашние задания: {self.average_rating}\nКурсы в процессе изучения: {self.courses_in_progress}\nЗавершенные курсы: {self.finished_courses}'
         return for_student
 
+    def __lt__(self, other):
+        if not isinstance(other, Student):
+            print('Not a Student')
+            return
+        return self.average_rating > other.average_rating
+
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
@@ -35,21 +43,21 @@ class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.teach_grades = {}
-        self.average_rating = 0
+        self.average_l_rating = 0
 
     def average_lectors_rate(self):
-        self.average_rating += round((sum(sum(self.teach_grades.values(), [])) / len(sum(self.teach_grades.values(), []))), 2)
-        return self.average_rating
+        self.average_l_rating += round((sum(sum(self.teach_grades.values(), [])) / len(sum(self.teach_grades.values(), []))), 2)
+        return self.average_l_rating
 
     def __str__(self):
-        for_lecturer = f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {self.average_lectors_rating}'
+        for_lecturer = f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {self.average_l_rating}'
         return for_lecturer
 
     def __lt__(self, other):
-        if not isinstance(other, Student):
-            print('Not a Student')
+        if not isinstance(other, Lecturer):
+            print('Not a Lecturer')
             return
-        return self.average_rating > other.average_rating
+        return self.average_l_rating > other.average_l_rating
 
 
 class Reviewer(Mentor):
@@ -67,34 +75,66 @@ class Reviewer(Mentor):
         return for_reviewer
 
 
-cool_reviewer = Reviewer('Mila', 'Jonas')
-cool_reviewer.courses_attached += ['Python']
+first_reviewer = Reviewer('Mila', 'Jonas')
+first_reviewer.courses_attached += ['Python']
+second_reviewer = Reviewer('Chris', 'Jacoby')
+second_reviewer.courses_attached += ['GIT', 'SQL']
 
-best_student = Student('Nastya', 'Fil', 'female')
-best_student.courses_in_progress += ['Python', 'SQL']
-best_student.finished_courses += ['GIT']
-best_student.grades["Python"] = [10, 10, 10]
-best_student.grades["SQL"] = [9, 9, 10]
 
-cool_lecturer = Lecturer('Mike', 'Mikelson')
-cool_lecturer.courses_attached += ['GIT']
+first_student = Student('Nastya', 'Fil', 'female')
+first_student.courses_in_progress += ['Python', 'GIT']
+first_student.finished_courses += ['SQL']
 
-cool_reviewer.rate_hw(best_student, 'Python', 10)
-# print(f'Оценка студенту {best_student.name} {best_student.surname}: {best_student.grades}')
+second_student = Student('Kate', 'Rodacheva', 'female')
+second_student.courses_in_progress += ['Python', 'GIT']
+second_student.finished_courses += []
 
-best_student.rate_for_lecturer(cool_lecturer, 'GIT', 10)
-# print(f'Оценка лектору {cool_lecturer.name} {cool_lecturer.surname} : {cool_lecturer.teach_grades}')
-cool_lecturer.teach_grades["Python"] = [10, 8, 10]
-cool_lecturer.teach_grades["GIT"] = [10, 10, 10]
 
-best_student.average_rate()
-cool_lecturer.average_lectors_rate()
+first_lecturer = Lecturer('Mike', 'Mikelson')
+first_lecturer.courses_attached += ['GIT']
 
-# print(cool_reviewer)
-# print(cool_lecturer)
-# print(best_student)
+second_lecturer = Lecturer('Alex', 'Romanov')
+second_lecturer.courses_attached += ['Python']
 
-print(cool_lecturer.average_rating < best_student.average_rating)
+
+first_reviewer.rate_hw(first_student, 'Python', 10)
+first_reviewer.rate_hw(first_student, 'Python', 9)
+first_reviewer.rate_hw(second_student, 'Python', 10)
+first_reviewer.rate_hw(second_student, 'Python', 10)
+second_reviewer.rate_hw(first_student, 'GIT', 10)
+second_reviewer.rate_hw(first_student, 'GIT', 10)
+second_reviewer.rate_hw(first_student, 'GIT', 10)
+second_reviewer.rate_hw(second_student, 'GIT', 10)
+second_reviewer.rate_hw(second_student, 'GIT', 8)
+
+
+first_student.rate_for_lecturer(first_lecturer, 'GIT', 10)
+first_student.rate_for_lecturer(first_lecturer, 'GIT', 10)
+first_student.rate_for_lecturer(second_lecturer, 'Python', 9)
+first_student.rate_for_lecturer(second_lecturer, 'Python', 9)
+second_student.rate_for_lecturer(first_lecturer, 'GIT', 9)
+second_student.rate_for_lecturer(first_lecturer, 'GIT', 10)
+second_student.rate_for_lecturer(second_lecturer, 'Python', 10)
+second_student.rate_for_lecturer(second_lecturer, 'Python', 10)
+
+
+first_student.average_rate()
+second_student.average_rate()
+first_lecturer.average_lectors_rate()
+second_lecturer.average_lectors_rate()
+
+# print(first_student)
+# print(second_student)
+# print(first_lecturer)
+# print(second_lecturer)
+# print(first_reviewer)
+# print(second_reviewer)
+
+# print(first_student > second_student)
+# print(first_lecturer < second_lecturer)
+
+
+
 
 
 
